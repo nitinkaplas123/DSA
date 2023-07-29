511a2e624ae97db9b2d3b741225fc6b55d0c769f
nitinkaplas <nitin.kaplas1@tcs.com>
2023-07-29 16:26:08 +0530

    checking

---
+++ tut2.cpp
@@ -0,0 +1,17 @@
+int print2largest(int arr[], int n) {
+	   int first=-1;
+	   int second=-1;
+	   for(int i=0;i<n;i++)
+	   {
+	       if(arr[i]>first)
+	       {
+	           second=first;
+	           first=arr[i];
+	       }
+	       else if(arr[i]<first and arr[i]>second)
+	       second=arr[i];
+	       else
+	       continue;
+	   }
+	   return second;
+	}
